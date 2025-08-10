d8bc601f7310a936e6e17846fddc73f0bca5494b
sibelephant <masterpiekhalid@gmail.com>
2025-08-10 21:58:36 +0100

    Enhance server shutdown process with timeout and ensure Redis closure

--- application/app.go
+++ application/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -32,6 +33,12 @@ func (a *App) Start(ctx context.Context) error {
 		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
 
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
 	fmt.Println("Starting server")
 
 	ch := make(chan error, 1)
@@ -48,6 +55,8 @@ func (a *App) Start(ctx context.Context) error {
 	case err = <-ch:
 		return err
 	case <-ctx.Done():
-		return server.Shutdown(context.Background())
+		timeout, cancel := context.WithTimeout(context.Background(),time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
 	}
 }
